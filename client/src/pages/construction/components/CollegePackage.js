c3371f520cb49217f3feb9c4fadc096432ab169b
Aniketh <anikethnair97@gmail.com>
2019-08-19 22:35:43 +0530

    College Package Sections

--- client/src/pages/construction/components/CollegePackage.js
+++ client/src/pages/construction/components/CollegePackage.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import { Card, Row, Col, Button, Tooltip, Tabs} from 'antd';
-import { HashLink as Link } from 'react-router-hash-link';
+import { Card, Row, Col, Button, InputNumber, Tabs, Radio, Switch    } from 'antd';
+import '../../../static/css/construction/Package.scss';
 import PackageLayout from '../../../components/PackageLayout';
+import phone from '../../../static/images/icons/phone-icon.svg';
+import service from '../../../static/images/service.svg';
+import delivery from '../../../static/images/delivery.svg';
+import quality from '../../../static/images/quality.svg';
+
+import Box from '@material-ui/core/Box';
 const { TabPane } = Tabs;
 const { Meta } = Card;
 
@@ -29,7 +34,6 @@ const itemList = [
 		price: '650 INR | unit',
 	}
 ];
-
 function Items() {
 	return (
 		<Row gutter={8}>
@@ -55,15 +59,32 @@ function Items() {
 								title={item.title}
 								description={item.price}
 							/>
+							<Radio></Radio>
 						</Card>
 				</Col>
 			))}
 		</Row>
 	);
 }
-function Faqs(){
-    return(
+
+function Includes() {
+	return (
 		<div>
+			<p>1 selected</p>
+			<Items />
+		</div>
+	);
+}
+function Offers() {
+	return (
+		<div style={{marginLeft:'20px'}}>
+			<h2>Offers</h2>
+		</div>
+	)
+}
+function Faqs() {
+	return (
+		<div style={{marginLeft:'30px'}}>
 			<h2>The Process</h2>
 			<h3>Question 1</h3>
 			<p>Answer</p>
@@ -78,115 +99,83 @@ function Faqs(){
 		</div>
 	)
 }
-function Addons(){
-    return (
-		<div>
-			<Row gutter={24} style={{marginBottom:'10px'}}>
-				<Col xs={24} sm={12} md={24} lg={12}>
-				<h2>Essentials</h2>
-				<p>Essentials are items that go really well with the main equipment that you rent out</p>
-				</Col>
-				<Col xs={24} sm={12} md={24} lg={12}>
-				<Paper style={{padding:'20px'}}>
-					<Row gutter={24}>
-						<Col xs={12}>
-						<h4>Select all</h4>
-						<p>Avail 10% discount!</p>
-						</Col>
-						<Col xs={12}>
-						Select
-						</Col>
-					</Row>
-				</Paper>
-				</Col>
-			</Row>
-            <Items/>
+function PackageSummary() {
+	return (
+		<div className="package-big">
+			<h1 className="package-header">College Package</h1>
+			<p className="package-message">Colleges across Mumbai hosting some of the best fests,
+				 workshops and other such event experiences. To help you make your college event stand out,
+				  book this package now!</p>
+			<div className="package-button-row">
+				<Button className="package-call-button"><img className="package-call-icon" src={phone} width="18" alt="Phone"/> Book now</Button>
+			</div>
+			<div style={{marginTop:'40px'}}>
+				<Row>
+					<Col xs={8} sm={6}>
+				<img src={service} alt="Customer Care Person"/>
+					</Col>
+					<Col xs={16} sm={18}>
+					<p>Colleges across Mumbai hosting some of the best fests,
+				 workshops and other such event experiences.</p>
+					</Col>
+				</Row>
+				<Row style={{marginTop:'40px'}}>
+					<Col xs={8} sm={6}>
+				<img src={delivery} alt="Delivery Truck"/>
+					</Col>
+					<Col xs={16} sm={18}>
+					<p>Colleges across Mumbai hosting some of the best fests,
+				 workshops and other such event experiences.</p>
+					</Col>
+				</Row>
+				<Row style={{marginTop:'40px'}}>
+					<Col xs={8} sm={6}>
+				<img src={quality} alt="5 Star Tool"/>
+					</Col>
+					<Col xs={16} sm={18}>
+					<p>Colleges across Mumbai hosting some of the best fests,
+				 workshops and other such event experiences.</p>
+					</Col>
+				</Row>
+			</div>
 		</div>
-	)
+	);
 }
-export default function CollegePackage  (){
-    return(
-        <PackageLayout>
-        <div className="header-construction-page">
-                <div className="product-wrapper">
-                        <Row gutter={8}>
-                            <Col xs={24} md={12} lg={8} className="package-details-wrapper">
-                                <div className="package-image-wrapper">
-                                    <img
-                                        width="100%"
-                                        alt="example"
-                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                                    />
-                                </div>
-                                <Box className="package-small-wrapper">
-                                    <div className="package-small">
-                                    <h1 style={{ marginBottom: '20px' }}>College Package</h1>
-									<Link to="/#partners">
-										Test
-									</Link>
-                                    <p>In this era of festivities, colleges around Mumbai are leading the country in some of the best events, fests and workshops ever hosted. If you think you have 
-                                    a desire to make your college event incredible book this package and let us help you become an incredible host!</p>
-                                    <div className="button-row">
-                                        <Row
-                                            gutter={16}
-                                        >
-                                            <Col
-                                                xs={24}
-                                                className="gutter-row"
-                                            >  <Tooltip title="Grab it now!" trigger="hover">
-                                                    <Button>Book Now</Button>
-                                                </Tooltip>
-                                            </Col>
-                                        </Row>
-                                    </div>
-                                </div>
-                                </Box>
-                                <div className="product-details">
-                                    <Tabs type="card">
-                                        <TabPane tab="Details" key="1">
-                                            <h2>Equipment Details</h2>
-                                            description
-                                            <h2>Features</h2>
-                                            <ul>
-                                                <li>Feature</li>
-                                                <li>Feature</li>
-                                                <li>Feature</li>
-                                                <li>Feature</li>
-                                            </ul>
-                                        </TabPane>
-                                        <TabPane tab="Add ons" key="2">
-                                            <Addons/>
-                                    </TabPane>
-                                        <TabPane tab="FAQs" key="3">
-                                            <Faqs/>
-                                    </TabPane>
-                                    </Tabs>
+class CollegePackage extends React.Component {
+	render() {
+		return (
+			<PackageLayout>
+				<div className="package-page-wrapper">
+					<div className="package-wrapper">
+						<Row gutter={8} style={{marginRight:'0'}}>
+							<Col xs={24} md={12} lg={14} className="package-details-wrapper">
+							<Box className="package-small-wrapper">
+									<PackageSummary />
+								</Box>
+								<div className="package-details">
+									<Tabs defaultActiveKey="0">
+										<TabPane tab="Includes" key="0">
+											<Includes />
+										</TabPane>
+										<TabPane tab="Offers" key="1">
+											<Offers />
+										</TabPane>
+										<TabPane tab="FAQs" key="2">
+											<Faqs />
+										</TabPane>
+									</Tabs>
 
-                                </div>
-                            </Col>
-                            <Col xs={0} md={12} lg={12} className="package-big-wrapper">
-                                <div className="package-big">
-                                    <h1 style={{ marginBottom: '20px' }}>College Package</h1>
-                                    <p>In this era of festivities, colleges around Mumbai are leading the country in some of the best events, fests and workshops ever hosted. If you think you have 
-                                    a desire to make your college event incredible book this package and let us help you become an incredible host!</p>
-                                    <div className="button-row">
-                                        <Row
-                                            gutter={16}
-                                        >
-                                            <Col
-                                                xs={24}
-                                                className="gutter-row"
-                                            >  <Tooltip title="Grab it now!" trigger="hover">
-                                                    <Button>Book Now</Button>
-                                                </Tooltip>
-                                            </Col>
-                                        </Row>
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-        </div>
-        </PackageLayout>
-    )
+								</div>
+							</Col>
+							<Col xs={0} md={12} lg={10} className="package-big-wrapper">
+								<PackageSummary />
+							</Col>
+						</Row>
+					</div>
+				</div>
+			</PackageLayout>
+		);
+	}
 }
+
+export default CollegePackage;
