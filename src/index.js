c84d31b422f00ae1fc0d81da4c1737c655d3a2bd
Aishwarya <ishans632@gmail.com>
2019-09-08 19:46:21 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ const express           = require('express');
 const cors              = require('cors'); 
 const path              = require('path'); 
 const db                = require('./db/index');
-const mainRoutes        = require('./routes/main.routes'); 
+const bodyParser        = require('body-parser'); 
+const mainRoutes        = require('./routes/main.routes');
+
 const port              = process.env.PORT || 5000; 
 
 const app = express(); 
 
 app.use(cors()); 
 app.use('/api',mainRoutes); 
+app.use(bodyParser.urlencoded({extended: true})); 
+
 app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')); 
-})
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../' ,'client', 'build'))); 
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+    }); 
+}
 
 app.listen(port, () => {
     console.log(`Listening to port: ${port}`); 
